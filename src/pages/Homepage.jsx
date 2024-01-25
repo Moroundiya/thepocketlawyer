17a1beb4c5696737026dffe820e3fa0510752235
MOROUNDIYA <moroundiya@outlook.com>
2024-01-25 04:48:02 +0100

    React template conversion

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import ToggleTheme from "@qvgk/react-toggle-theme";
 import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react';
 import 'animate.css';
 import '../custom.js'
+import '../App.css';
 
-import logo from '../assets/images/logo.png';
+import logo_light from '../assets/images/logo-light.png';
+import logo_dark from '../assets/images/logo-dark.png';
 import logo4 from '../assets/images/logo4.png';
 import logo5 from '../assets/images/logo5.png';
-import banner from '../assets/images/banner.png';
-import sliderImg from '../assets/images/slider_img.png';
 import sliderbg from '../assets/images/sliderbg.png';
-import abt_1 from '../assets/images/abt_1.png';
 import abt_2 from '../assets/images/abt_2.png';
 import abt_3 from '../assets/images/abt_3.png';
 import video_bg from '../assets/images/video_bg.jpg';
@@ -24,9 +24,6 @@ import mem3 from '../assets/images/mem3.png';
 import mem4 from '../assets/images/mem4.png';
 import mem5 from '../assets/images/mem5.png';
 import mem6 from '../assets/images/mem6.png';
-import workBanner from '../assets/images/work_banner.jpg';
-import ts1 from '../assets/images/ts1.png';
-import ts2 from '../assets/images/ts2.png';
 import inves1 from '../assets/images/inves1.png';
 import inves2 from '../assets/images/inves2.png';
 import inves3 from '../assets/images/inves3.png';
@@ -153,10 +150,10 @@ function Homepage() {
                 {/* .cd-dropdown-content */}
             </nav>
             <div className="cp_navi_main_wrapper index2_header_wrapper float_left">
-                <div className="container-fluid">
+                <div className="container-fluid header-parent-container">
                     <div className="cp_logo_wrapper">
                         <a href="index.html">
-                            <img src={currentTheme === "light" ? logo4 : logo5} alt="logo" />
+                            <img src={currentTheme === "light" ? logo_dark : logo_light} alt="logo" className='header-logo' />
                         </a>
                     </div>
                     {/* mobile menu area start */}
@@ -165,176 +162,63 @@ function Homepage() {
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="cd-dropdown-wrapper">
-                                        <a className="house_toggle" href="#0">
-                                            <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 31.177 31.177" style={{ enableBackground: 'new 0 0 31.177 31.177' }} xmlSpace="preserve" width="25px" height="25px">
-                                                <g>
-                                                    <g>
-                                                        <path className="menubar" d="M30.23,1.775H0.946c-0.489,0-0.887-0.398-0.887-0.888S0.457,0,0.946,0H30.23    c0.49,0,0.888,0.398,0.888,0.888S30.72,1.775,30.23,1.775z" fill="#004165" />
-                                                    </g>
-                                                    <g>
-                                                        <path className="menubar" d="M30.23,9.126H12.069c-0.49,0-0.888-0.398-0.888-0.888c0-0.49,0.398-0.888,0.888-0.888H30.23    c0.49,0,0.888,0.397,0.888,0.888C31.118,8.729,30.72,9.126,30.23,9.126z" fill="#004165" />
-                                                    </g>
-                                                    <g>
-                                                        <path className="menubar" d="M30.23,16.477H0.946c-0.489,0-0.887-0.398-0.887-0.888c0-0.49,0.398-0.888,0.887-0.888H30.23    c0.49,0,0.888,0.397,0.888,0.888C31.118,16.079,30.72,16.477,30.23,16.477z" fill="#004165" />
-                                                    </g>
-                                                    <g>
-                                                        <path className="menubar" d="M30.23,23.826H12.069c-0.49,0-0.888-0.396-0.888-0.887c0-0.49,0.398-0.888,0.888-0.888H30.23    c0.49,0,0.888,0.397,0.888,0.888C31.118,23.43,30.72,23.826,30.23,23.826z" fill="#004165" />
-                                                    </g>
-                                                    <g>
-                                                        <path className="menubar" d="M30.23,31.177H0.946c-0.489,0-0.887-0.396-0.887-0.887c0-0.49,0.398-0.888,0.887-0.888H30.23    c0.49,0,0.888,0.398,0.888,0.888C31.118,30.78,30.72,31.177,30.23,31.177z" fill="#004165" />
-                                                    </g>
-                                                </g>
-                                            </svg>
-                                        </a>
-                                        {/* .cd-dropdown */}
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                         {/* .cd-dropdown-wrapper */}
                     </header>
-                    <div className="top_header_right_wrapper top_phonecalls">
-                        <p><i className="flaticon-phone-contact" /> (+91) 123 123 4567</p>
-                        <div className="header_btn">
-                            <ul>
-                                <li>
-                                    <a href="register.html"> register </a> </li>
-                                <li>
-                                    <a href="login.html"> login </a> </li>
-                            </ul>
-                        </div>
-                        <ToggleTheme selectedTheme={currentTheme} onChange={setCurrentTheme} />
 
-                    </div>
                     <div className="cp_navigation_wrapper">
                         <div className="mainmenu d-xl-block d-lg-block d-md-none d-sm-none d-none">
                             <ul className="main_nav_ul">
-                                <li className="has-mega gc_main_navigation"><a href="#" className="gc_main_navigation active_class">index <i className="fas fa-caret-down" /></a>
-                                    <ul className="navi_2_dropdown">
-                                        <li className="parent">
-                                            <a href="index.html"><i className="fas fa-caret-right" />index I</a>
-                                        </li>
-                                        <li className="parent">
-                                            <a href="index2.html"><i className="fas fa-caret-right" />index II</a>
-                                        </li>
-                                        <li className="parent">
-                                            <a href="index3.html"><i className="fas fa-caret-right" />index III</a>
-                                        </li>
-                                    </ul>
+                                <li className="has-mega gc_main_navigation">
+                                    <a href="#" className="gc_main_navigation active_class">HOME
+                                    </a>
                                 </li>
-                                <li><a href="about_us.html" className="gc_main_navigation">about us</a></li>
-                                <li><a href="investment.html" className="gc_main_navigation">investment plan</a></li>
-                                <li className="has-mega gc_main_navigation"><a href="#" className="gc_main_navigation">pages <i className="fas fa-caret-down" /></a>
-                                    <ul className="navi_2_dropdown">
-                                        <li className="parent">
-                                            <a href="faq.html"><i className="fas fa-caret-right" />FAQ</a>
-                                        </li>
-                                        <li className="parent">
-                                            <a href="login.html"><i className="fas fa-caret-right" />login</a>
-                                        </li>
-                                        <li className="parent">
-                                            <a href="register.html"><i className="fas fa-caret-right" />register</a>
-                                        </li>
-                                    </ul>
+                                <li><a href="about_us.html" className="gc_main_navigation">ABOUT US</a></li>
+                                <li className="has-mega gc_main_navigation">
+                                    <a href="#" className="gc_main_navigation">PRICING
+                                    </a>
+
                                 </li>
-                                <li className="has-mega gc_main_navigation"><a href="#" className="gc_main_navigation">dashboard <i className="fas fa-caret-down" /></a>
-                                    <ul className="navi_2_dropdown">
-                                        <li className="parent">
-                                            <a href="#"><i className="fas fa-caret-right" />my account<span><i className="fas fa-caret-right" />
-                                            </span></a>
-                                            <ul className="dropdown-menu-right">
-                                                <li>
-                                                    <a href="my_account.html"> <i className="fas fa-caret-right" />my account </a>
-                                                </li>
-                                                <li>
-                                                    <a href="view_profile.html"> <i className="fas fa-caret-right" /> my profile</a>
-                                                </li>
-                                                <li>
-                                                    <a href="email_notification.html"><i className="fas fa-caret-right" />email notification </a>
-                                                </li>
-                                                <li>
-                                                    <a href="change_password.html"><i className="fas fa-caret-right" />change password</a>
-                                                </li>
-                                                <li>
-                                                    <a href="change_pin.html"><i className="fas fa-caret-right" />change pin</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li className="parent">
-                                            <a href="#"> <i className="fas fa-caret-right" />finance<span> <i className="fas fa-caret-right" />
-                                            </span></a>
-                                            <ul className="dropdown-menu-right">
-                                                <li>
-                                                    <a href="make_deposit.html"> <i className="fas fa-caret-right" />make deposit</a>
-                                                </li>
-                                                <li>
-                                                    <a href="deposit_list.html"> <i className="fas fa-caret-right" /> deposit lists</a>
-                                                </li>
-                                                <li>
-                                                    <a href="payment_request.html"><i className="fas fa-caret-right" />payment request</a>
-                                                </li>
-                                                <li>
-                                                    <a href="exchange_money.html"><i className="fas fa-caret-right" />exchange money</a>
-                                                </li>
-                                                <li>
-                                                    <a href="transfer_fund.html"><i className="fas fa-caret-right" />fund transfer</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li className="parent">
-                                            <a href="#"> <i className="fas fa-caret-right" />reports<span> <i className="fas fa-caret-right" />
-                                            </span></a>
-                                            <ul className="dropdown-menu-right">
-                                                <li>
-                                                    <a href="all_transactions.html"> <i className="fas fa-caret-right" />all transactions</a>
-                                                </li>
-                                                <li>
-                                                    <a href="deposit_history.html"> <i className="fas fa-caret-right" /> deposit history</a>
-                                                </li>
-                                                <li>
-                                                    <a href="pending_history.html"><i className="fas fa-caret-right" />pending history</a>
-                                                </li>
-                                                <li>
-                                                    <a href="exchange_history.html"><i className="fas fa-caret-right" />exchange history</a>
-                                                </li>
-                                                <li>
-                                                    <a href="earnings_history.html"><i className="fas fa-caret-right" />earning history</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li className="parent">
-                                            <a href="#"> <i className="fas fa-caret-right" />referrals<span> <i className="fas fa-caret-right" />
-                                            </span></a>
-                                            <ul className="dropdown-menu-right">
-                                                <li>
-                                                    <a href="referrals.html"> <i className="fas fa-caret-right" />my referrals</a>
-                                                </li>
-                                                <li>
-                                                    <a href="banners.html"> <i className="fas fa-caret-right" /> promotionals banners</a>
-                                                </li>
-                                                <li>
-                                                    <a href="referral_earnings.html"><i className="fas fa-caret-right" />referral earnings</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li className="parent">
-                                            <a href="tickets.html"><i className="fas fa-caret-right" />view tickets</a></li>
-                                    </ul>
+                                <li className="has-mega gc_main_navigation">
+                                    <a href="#" className="gc_main_navigation">FAQ
+                                    </a>
+
                                 </li>
-                                <li className="has-mega gc_main_navigation"><a href="#" className="gc_main_navigation">blog <i className="fas fa-caret-down" /></a>
-                                    <ul className="navi_2_dropdown">
-                                        <li className="parent">
-                                            <a href="blog_category.html"><i className="fas fa-caret-right" />blog category</a>
-                                        </li>
-                                        <li className="parent">
-                                            <a href="blog_single.html"><i className="fas fa-caret-right" /> blog single</a>
-                                        </li>
-                                    </ul>
+                                <li className="has-mega gc_main_navigation">
+                                    <a href="#" className="gc_main_navigation">BLOG
+                                    </a>
+
                                 </li>
-                                <li><a href="contact_us.html" className="gc_main_navigation">contact us</a></li>
+                                <li>
+                                    <a href="contact_us.html" className="gc_main_navigation">CONTACT US </a>
+                                </li>
                             </ul>
                         </div>
                         {/* mainmenu end */}
+                    </div>
+
+                    <div className="top_header_right_wrapper top_phonecalls register-login-container">
+                        {/* <p className='no_text'>
+                            <i className="flaticon-phone-contact" /> (+234) 000 000 0000
+                        </p> */}
+                        <div className="header_btn header-btn-div">
+                            <ul>
+                                <li>
+                                    <a href="register.html">
+                                        <Icon icon="fa6-solid:user-clock" className='book-icon' />
+                                        Book Appointment
+                                    </a> </li>
+                                <li>
+                                    <a href="login.html">
+                                        <Icon icon="carbon:password" className='signin-icon' />
+                                        Sign In </a> </li>
+                            </ul>
+                        </div>
+                        <ToggleTheme selectedTheme={currentTheme} onChange={setCurrentTheme} />
                     </div>
                 </div>
             </div>
